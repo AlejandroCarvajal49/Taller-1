6b86d4e8ef8d19a467422d61ac3f5eefddce338b
AlejandroCarvajal49 <a.carvajalt@uniandes.edu.co>
2024-08-11 19:31:36 -0500

    Taller 1 con los requerimientos establecidos

---
+++ tests/uniandes/dpoo/gasolinera/tests/EmpleadoTest.java
@@ -0,0 +1,46 @@
+package uniandes.dpoo.gasolinera.tests;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import uniandes.dpoo.gasolinera.logica.Empleado;
+import org.junit.jupiter.api.Assertions;
+
+public class EmpleadoTest
+{
+    private Empleado e1;
+
+    @BeforeEach
+    void setUp( ) throws Exception
+    {
+        e1 = new Empleado( "e1" );
+    }
+
+    @AfterEach
+    void tearDown( ) throws Exception
+    {
+    }
+
+    @Test
+    void testEmpleado( )
+    {
+        Assertions.assertEquals( "e1", e1.getNombre( ), "El nombre del empleado no es el correcto" );
+        Assertions.assertEquals( 0, e1.getCantidadDinero( ), "El nuevo empleado no debería tener dinero" );
+    }
+
+    @Test
+    void testAgregarDinero( )
+    {
+        assertEquals( 0, e1.getCantidadDinero( ), "El nuevo empleado no debería tener dinero" );
+        e1.agregarDinero( 100 );
+        assertEquals( 100, e1.getCantidadDinero( ), "El nuevo empleado ya debería tener dinero" );
+        e1.agregarDinero( 100 );
+        assertEquals( 200, e1.getCantidadDinero( ), "El nuevo empleado debería tener más dinero ahora" );
+    }
+
+}
